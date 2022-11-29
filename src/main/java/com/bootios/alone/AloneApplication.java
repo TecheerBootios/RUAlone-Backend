d920a695cef9c1bb1d6b394e9603f30e912ec16e
hi-june <wnsgur735@gmail.com>
2022-11-30 00:25:15 +0900

    chore: initial commit

---
+++ src/main/java/com/bootios/alone/AloneApplication.java
@@ -0,0 +1,13 @@
+package com.bootios.alone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootios/alone/AloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootios.alone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
