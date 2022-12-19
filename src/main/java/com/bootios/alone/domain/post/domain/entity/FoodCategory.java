3ef2ba8c2682f813a9371b9da75a982f8b623f6e
jung <mira20932@gmail.com>
2022-12-19 19:32:31 +0900

    feat: Post 엔티티 구현

---
+++ src/main/java/com/bootios/alone/domain/post/domain/entity/FoodCategory.java
@@ -0,0 +1,5 @@
+package com.bootios.alone.domain.post.domain.entity;
+
+public enum FoodCategory {
+    한식,중식,일식,치킨,분식
+}
---
+++ src/main/java/com/bootios/alone/domain/post/domain/entity/Post.java
@@ -0,0 +1,40 @@
+package com.bootios.alone.domain.post.domain.entity;
+
+import com.bootios.alone.domain.users.domain.entity.Users;
+import com.bootios.alone.global.common.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "post")
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creator_id", nullable = false)
+    private Users creator;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "start_at", nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(name = "limit_member", nullable = false)
+    private Integer limitMember;
+
+    @Column(name = "food_category", nullable = false) // 관리자 or 사용자 구분
+    @Enumerated(EnumType.STRING)
+    private FoodCategory foodCategory;
+
+}
