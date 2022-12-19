37950723356f1db9ba5a9f6989a0070b2e38de29
jung <mira20932@gmail.com>
2022-12-20 01:18:44 +0900

    feat: Post 생성자와 builder 추가

--- src/main/java/com/bootios/alone/domain/post/domain/entity/Post.java
+++ src/main/java/com/bootios/alone/domain/post/domain/entity/Post.java
@@ -3,12 +3,12 @@ package com.bootios.alone.domain.post.domain.entity;
 import com.bootios.alone.domain.users.domain.entity.Users;
 import com.bootios.alone.global.common.BaseEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-
 
 @Getter
 @Entity
@@ -16,25 +16,38 @@ import java.time.LocalDateTime;
 @Table(name = "post")
 public class Post extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "creator_id", nullable = false)
-    private Users creator;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "creator_id", nullable = false)
+  private Users creator;
 
-    @Column(name = "title", nullable = false)
-    private String title;
+  @Column(name = "title", nullable = false)
+  private String title;
 
-    @Column(name = "start_at", nullable = false)
-    private LocalDateTime startAt;
+  @Column(name = "start_at", nullable = false)
+  private LocalDateTime startAt;
 
-    @Column(name = "limit_member", nullable = false)
-    private Integer limitMember;
+  @Column(name = "limit_member", nullable = false)
+  private Integer limitMember;
 
-    @Column(name = "food_category", nullable = false) // 관리자 or 사용자 구분
-    @Enumerated(EnumType.STRING)
-    private FoodCategory foodCategory;
+  @Column(name = "food_category", nullable = false) // 관리자 or 사용자 구분
+  @Enumerated(EnumType.STRING)
+  private FoodCategory foodCategory;
 
+  @Builder
+  public Post(
+      Users creator,
+      String title,
+      LocalDateTime startAt,
+      Integer limitMember,
+      FoodCategory foodCategory) {
+    this.creator = creator;
+    this.title = title;
+    this.startAt = startAt;
+    this.limitMember = limitMember;
+    this.foodCategory = foodCategory;
+  }
 }
