70d93ea3b5bd35de0b570c85b3725f1c15811581
kimich <ickim1218@gmail.com>
2022-12-21 00:32:16 +0900

    feat: errorCode, resultCode을 관리하는 enum 클래스 생성

---
+++ src/main/java/com/bootios/alone/global/result/ResultCode.java
@@ -0,0 +1,21 @@
+package com.bootios.alone.global.result;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/** {행위}_{목적어}_{성공여부} message 는 동사 명사형으로 마무리 */
+@Getter
+@AllArgsConstructor
+public enum ResultCode {
+
+  // 도메인 별로 나눠서 관리(ex: User 도메인)
+  // user
+
+
+  // post
+
+ ;
+
+  private final String code;
+  private final String message;
+}
