cf941a6902966c3f43f39c0716d2004e634e7f7c
kimich <ickim1218@gmail.com>
2022-12-26 23:34:24 +0900

    feat: location entity 및 repository 생성

---
+++ src/main/java/com/bootios/alone/domain/location/domain/entity/Location.java
@@ -0,0 +1,36 @@
+package com.bootios.alone.domain.location.domain.entity;
+
+import com.bootios.alone.domain.post.domain.entity.Post;
+import com.bootios.alone.global.common.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "location")
+public class Location extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private Float xCoordinate;
+
+    @Column(nullable = false)
+    private Float yCoordinate;
+
+    @OneToOne(mappedBy = "location")
+    private Post post;
+
+    @Builder
+    public Location(Float xCoordinate, Float yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+}
---
+++ src/main/java/com/bootios/alone/domain/location/domain/repository/LocationRepository.java
@@ -0,0 +1,7 @@
+package com.bootios.alone.domain.location.domain.repository;
+
+import com.bootios.alone.domain.location.domain.entity.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository<Location, Long> {
+}
