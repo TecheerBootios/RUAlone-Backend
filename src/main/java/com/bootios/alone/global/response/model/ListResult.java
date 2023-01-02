f72a6eb7ba79dc61cda1eebd0c88f5166bd973d3
hi-june <wnsgur735@gmail.com>
2023-01-03 00:20:38 +0900

    feat: 3가지 공통 response 모델 추가

---
+++ src/main/java/com/bootios/alone/global/response/model/ListResult.java
@@ -0,0 +1,12 @@
+package com.bootios.alone.global.response.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ListResult<T> extends CommonResult {
+    private List<T> list;
+}
---
+++ src/main/java/com/bootios/alone/global/response/model/SingleResult.java
@@ -0,0 +1,10 @@
+package com.bootios.alone.global.response.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SingleResult<T> extends CommonResult {
+    private T data;
+}
