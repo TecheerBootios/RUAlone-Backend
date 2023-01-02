93ae8a5537190ffe8782c6757c8f3ba3c394b663
hi-june <wnsgur735@gmail.com>
2023-01-03 00:55:20 +0900

    feat: token 기반의 sign in, sign up 기능 추가

---
+++ src/main/java/com/bootios/alone/domain/user/dto/request/UserLoginRequestDto.java
@@ -0,0 +1,24 @@
+package com.bootios.alone.domain.user.dto.request;
+
+import com.bootios.alone.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserLoginRequestDto {
+    private String email;
+    private String password;
+
+    public User toUser(PasswordEncoder passwordEncoder) {
+        return User.builder()
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .build();
+    }
+}
