42b95041626c4a8f9c71ebfc11170a4a2bfff82c
hi-june <wnsgur735@gmail.com>
2023-01-03 00:50:46 +0900

    feat: token 기반의 User service 작성

---
+++ src/main/java/com/bootios/alone/domain/user/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.bootios.alone.domain.user.service;
+
+import com.bootios.alone.domain.user.repository.UserRepository;
+import com.bootios.alone.global.advice.exception.CUserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String userPk) throws UsernameNotFoundException {
+        return userRepository.findById(Long.parseLong(userPk))
+                .orElseThrow(CUserNotFoundException::new);
+    }
+}
