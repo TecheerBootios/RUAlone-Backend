86e60b03d44cfb6aee4c4cedc1802d9d2c26c71f
kimich <ickim1218@gmail.com>
2023-01-10 12:44:09 +0900

    feat: 좌표값 저장하는 Location Service 구현

---
+++ src/main/java/com/bootios/alone/domain/location/service/LocationService.java
@@ -0,0 +1,28 @@
+package com.bootios.alone.domain.location.service;
+
+import com.bootios.alone.domain.location.domain.entity.Location;
+import com.bootios.alone.domain.location.domain.repository.LocationRepository;
+import com.bootios.alone.domain.location.dto.LocationCreateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class LocationService {
+
+    public final LocationRepository locationRepository;
+
+    public void registerLocation(LocationCreateRequest locationCreateRequest) {
+
+        Location location = createRequestToEntity(locationCreateRequest);
+
+        locationRepository.save(location);
+    }
+
+    public Location createRequestToEntity(LocationCreateRequest locationCreateRequest) {
+        return Location.builder()
+                .xCoordinate(locationCreateRequest.getXxCoordinate())
+                .yCoordinate(locationCreateRequest.getYyCoordinate())
+                .build();
+    }
+}
