f6cabf9afefade647288bf151533b69c5e9be331
baejunil <bjo6300@naver.com>
2023-01-25 15:50:54 +0900

    feat : location 조회 기능 JPQL 추가

--- src/main/java/com/bootios/alone/domain/location/repository/LocationRepository.java
+++ src/main/java/com/bootios/alone/domain/location/repository/LocationRepository.java
@@ -1,9 +1,12 @@
 package com.bootios.alone.domain.location.repository;
 
 import com.bootios.alone.domain.location.entity.Location;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
-  Optional<Location> findLocationByPostId(Long id);
+    @Query("select l from Location l where l.post.id = :id and l.isActive = true")
+    Optional<Location> findLocationByPostId(@Param("id") Long id);
 }
