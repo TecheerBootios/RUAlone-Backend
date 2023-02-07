f1edd19559d80aa9a53ce7cbd7d192c57cc00fbc
baejunil <bjo6300@naver.com>
2023-02-07 16:18:09 +0900

    fix : String이 아닌 타입 @NotNull로 수정
    숫자, enum은 @NotBlank 불가능

--- src/main/java/com/bootios/alone/domain/post/dto/PostCreateRequest.java
+++ src/main/java/com/bootios/alone/domain/post/dto/PostCreateRequest.java
@@ -4,6 +4,8 @@ import com.bootios.alone.domain.post.entity.FoodCategory;
 import java.time.LocalDateTime;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,7 +15,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class PostCreateRequest {
 
-  @NotBlank(message = "userId는 공백이 올 수 없습니다.")
+  @NotNull(message = "userId는 공백이 올 수 없습니다.")
   private final Long creatorId;
 
   @NotBlank(message = "제목은 빈칸일 수 없습니다")
@@ -27,14 +29,14 @@ public class PostCreateRequest {
   @Min(value = 2, message = "혼밥하실 수 없습니다.")
   private final Integer limitMember;
 
-  @NotBlank(message = "카테고리는 공백이 될 수 없습니다.")
+  @NotNull(message = "userId는 공백이 올 수 없습니다.")
   private final FoodCategory foodCategory;
 
   @Min(value = 0, message = "위도는 0이하가 될 수 없습니다.")
-  @NotBlank(message = "위도는 공백이 될 수 없습니다.")
+  @NotNull(message = "userId는 공백이 올 수 없습니다.")
   private final Float latitude;
 
   @Min(value = 0, message = "경도는 0이하가 될 수 없습니다.")
-  @NotBlank(message = "경도는 공백이 될 수 없습니다.")
+  @NotNull(message = "userId는 공백이 올 수 없습니다.")
   private final Float longitude;
 }
